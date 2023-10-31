c55de0d122ff20c50480e35a5bcfbcebde70a4d4
M0nk3y <i4mhmh@outlook.com>
2023-10-31 12:23:59 +0800

    complete demo

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# A-custom-gredSearch-method-in-SCA
+# 基于深度学习的侧信道攻击中网格搜索的重新封装实现
+
+>   超参搜索demo，解决了原始封装的Grap包中metric的问题
+
+AI侧信道攻击的判定指标不是acc或者其他，并不需要predict来判定model performance，这里目前先将MR融合到search函数中，verbose=1观察MR的收敛曲线。
+
+*   使用环境：Python  	3.8.16
+*   tensorflow-macos      2.11.0
+*   keras                            2.10.0
+
+如下图返回的示例
+
+![返回图像](../../../../../Users/wink/blog/source/img/image-20231031122218257.png)
--- custom_callback.py
+++ custom_callback.py
@@ -4,25 +4,27 @@
 * File       : custom_callback.py
 * Created    : 2023-10-30 11:43:32
 * Author     : M0nk3y
-* Version    : 1.0
+* Version    : 1.1
 '''
 
 from sklearn.model_selection import ParameterGrid, train_test_split
 import matplotlib.pyplot as plt
 import numpy as np
+from keras import optimizers
 
 
-def show_result(rank_times):
-    fig, ax = plt.subplots(fig_size=(15, 7))
-    plt.title()
+def show_result(rank_times, optmizier, learning_rate, epoch, batch_size, loss):
+    fig, ax = plt.subplots(figsize=(15, 7))
+    plt.title("optmizer:" + str(optmizier) + '_' + str(learning_rate) + '_epoch:' + str(epoch) + '_batch' + str(batch_size) + '_loss: '+ str(loss))
     x = [x for x in range(0, 2000)]
     ax.plot(x, np.mean(rank_times, axis=0), 'b')
-    ax.set(xlabel="number of traces", ylabels="mean rank")
+    ax.set(xlabel="number of traces", ylabel="mean rank")
+    plt.show()
 
 
-def mean_rank_score(model, test_data, targets, verbose, pred_size, key):
-    attack_traces = test_data['attack_traces']
-    predictions = model.predict(attack_traces)
+def mean_rank_score(model, test_data, pred_size, key):
+    attack_traces, targets = test_data[0], test_data[1]
+    predictions = model.predict(attack_traces, verbose=0)
     predictions = np.log(predictions + 1e-40)
     #! 可能需要修改
     rank_times = np.zeros(pred_size)
@@ -37,26 +39,34 @@ def mean_rank_score(model, test_data, targets, verbose, pred_size, key):
 
 # 自定网格调参callbacks 暂时只修改model的param.
 def monkey_search(data, params, model, callbacks, key, shuffle=True, pred_size=2000, verbose=1):
-    assert type(callbacks) is list
+    assert type(callbacks) == list
     train_data, test_data = data['train_data'], data['test_data']
+    idx = None
+    for i, callback in enumerate(callbacks):
+        if callback.__class__.__name__ == 'OneCycleLR':
+            idx = i
 
     candidatas = ParameterGrid(param_grid=params)
     for index, candidata in enumerate(candidatas):
         x_train, x_val, y_train, y_val = train_test_split(train_data[0], train_data[1], test_size=0.2, shuffle=True) if(shuffle == True) else train_test_split(train_data[0], train_data[1], test_size=0.2)
-        optmizier = candidata['optimizer']
+        optimizer = candidata['optimizer']
         learning_rate = candidata['learning_rate']
         epoch = candidata['epoch']
         batch_size = candidata['batch_size']
         loss = candidata['loss']
-        model.complie(optmizier=optmizier(learning_rate), loss=loss, metrics=['acc'])
-        model.fit(x_train, y_train, validation_data=(x_val, y_val), callbacks=callbacks, epoch=epoch, batch_size=batch_size)
+        print(learning_rate, type(learning_rate))
+        # 如果有onecycle要给对应赋值
+        if idx != None:
+            callbacks[idx].num_samples = x_train.shape[0]
+            callbacks[idx].batch_size = batch_size
+            callbacks[idx].max_lr = learning_rate
+        model.compile(optimizer=optimizer, loss=loss, metrics=['acc'])
+        model.optimizer.learning_rate.assign(learning_rate)
+        model.fit(x_train, y_train, validation_data=(x_val, y_val), callbacks=callbacks, epochs=epoch, batch_size=batch_size)
 
-        ranks = np.zeros((100, pred_size))
+        ranks = np.zeros((10, pred_size))
         for i in range(ranks.shape[0]):
             ranks[i] = mean_rank_score(model, test_data, pred_size=pred_size, key=key)
-        if verbose is 1:
-            show_result(ranks, optmizier, learning_rate )
+        if verbose == 1:
+            show_result(ranks, optimizer, learning_rate, epoch, batch_size, loss)
 
-param_grid = {'optmizier': [1, 2], 'learning_rate': [True, False]}
-data = {'train_data': [1,2,3,4,5,6,7],  'test_data': [1,2]}
-monkey_search(data, param_grid, 2,[2])
